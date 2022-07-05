49acc1d9b597eaacc521f0cbb48441ab02de382a
Markpm-code <marlonseglos@yahoo.com>
2022-07-05 08:08:14 +0000

    Add update on javascript codes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,22 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them//
 
-
-
-document.addEventListeners("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked submit!");
-            } else {
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit"){
+                alert("You clicked Submit!");
+            }else {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
             }
         })
     }
 })
+
+           
 function runGame(){
 
 }
