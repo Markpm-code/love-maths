ccab71b6f8e40adc6a7e829c4202c4885af9a563
Markpm-code <marlonseglos@yahoo.com>
2022-07-19 21:21:59 +0000

    Add division question

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,8 +41,9 @@ function runGame(gameType){
         displayMultiplyQuestion(num1, num2);
     } else if(gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
-    }
-        else {
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
+    }   else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. Aborting!`
     }
@@ -124,4 +125,14 @@ function displayMultiplyQuestion(operand1, operand2){
     document.getElementById('operand1').textContent = operand1;
      document.getElementById('operand2').textContent = operand2;
      document.getElementById('operator').textContent = "x";
+}
+
+// Add your division question here
+function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2;
+    
+    document.getElementById("operand1").textContent = operand1;
+	document.getElementById("operand2").textContent = operand2;
+	document.getElementById("operator").textContent = "/";
+
 }
